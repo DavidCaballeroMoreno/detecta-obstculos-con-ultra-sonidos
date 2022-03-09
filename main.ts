a6f1d06de9c08cf605870c47c2ad8e0c156554bf
David Caballero Moreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-03-09 09:30:36 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+function girar90derecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+    basic.pause(260)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+function girar90izquierda () {
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
+    basic.pause(260)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+        girar90izquierda()
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 155)
+        basic.pause(1000)
+        girar90derecha()
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+    }
+})
